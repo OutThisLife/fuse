56bec396322d60c8afcfb4afb15487d977cf9c6a
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-01-29 11:55:02 -0600

    copy/paste

--- web/app/themes/fuse/single-neighborhood.php
+++ web/app/themes/fuse/single-neighborhood.php
@@ -67,7 +67,11 @@ the_post();
 
         <?php if ($schools = getSchools('neighborhood', get_the_ID())): ?>
         <div class="row school-info">
-            <h2>Schools</h2>
+            <?php
+            foreach($schools AS $type => $grouped):
+                $long = count($grouped) > 3;
+            ?>
+            <h2 style='text-transform: capitalize'><?=$type?></h2>
 
             <div class="school-row">
                 <div class="school-labels">
@@ -80,22 +84,29 @@ the_post();
                     </div>
                 </div>
 
-                <?php
-                foreach ($schools AS $type => $grouped)
-                    foreach ($grouped AS $school):
-                ?>
-                <div class="school">
-                    <div class="rating-name">
-                        <span class="rating"><?=$school['rating'] ?></span>
-                        <h6><?=$school['title']?></h6>
-                    </div>
+                <div class="school-wrapper <?=$long ? 'show-fade' : ''?>">
+                    <?php foreach ($grouped AS $school): ?>
+                    <div class="school">
+                        <div class="rating-name">
+                            <span class="rating"><?=$school['rating'] ?></span>
+                            <h6><?=$school['title']?></h6>
+                        </div>
 
-                    <div class="grades-distance">
-                        <div class="grades"><?=$school['grades']?></div>
+                        <div class="grades-distance">
+                            <div class="grades"><?=$school['grades']?></div>
+                        </div>
                     </div>
+                    <?php endforeach ?>
                 </div>
-                <?php endforeach ?>
 
+                <?php if ($long): ?>
+                <div class="expand-school expand-list" data-el=".school-wrapper">
+                    <a href="javascript:;">
+                        <i class="icon-down-arrow"></i>
+                        Expand all <?=$school_type['school_type']?>
+                    </a>
+                </div>
+                <?php endif ?>
 
                 <?php if ($school_copy = CFS()->get('schools_copy')): ?>
                 <div class="school-copy">
@@ -103,6 +114,7 @@ the_post();
                 </div>
                 <?php endif ?>
             </div>
+            <?php endforeach ?>
         </div>
         <?php endif ?>
 
