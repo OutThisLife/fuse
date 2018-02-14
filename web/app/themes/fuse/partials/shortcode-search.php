ffd31efb67e159247337e7b5235c732cadf11135
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-02-14 16:52:37 -0600

    make icon clickable

--- web/app/themes/fuse/partials/shortcode-search.php
+++ web/app/themes/fuse/partials/shortcode-search.php
@@ -1,4 +1,8 @@
 <form action='/search' id="search">
-  <input class="search" type="text" placeholder="Search by Neighborhood, School, or Zipcode" name='q' />
+  <div class='with-icon'>
+    <input class="search" type="text" placeholder="Search by Neighborhood, School, or Zipcode" name='q' />
+    <button class="btn-icon"><img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAWlBMVEX////yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/yix/IzHx9AAAAHXRSTlMAAQQGDhQqLFhlfIGQl6OrvL7Bw9fY7u/w8fL3++cpGMwAAACRSURBVHgBdZBbjoQgEAAZYUZxVfABi4+6/zWXBF2TjlNflQppaFSmsj4Sva3UzSdQCJ+7rZBc5xKsV60C9CaL6SGcE2xuqpCrLeZJ5owm4YtFnLpwxCLQ/ccOnk5uTzPHb7fLdy5abPQL+FruzuLZm9LOX9pGq+sd2pcSNMCgZW2B+S3iawAmJdAzHEryno6fP8U4DXMF2v3kAAAAAElFTkSuQmCC' /></button>
+  </div>
+
   <button class="btn">Search</button>
 </form>
