e1df1519387aedeb06c440fffa06e9dd6cb5f959
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-02-20 13:50:02 -0600

    add privacy policy page

--- web/app/themes/fuse/footer.php
+++ web/app/themes/fuse/footer.php
@@ -32,7 +32,7 @@
 
         <p>The information being provided is for consumers’ personal, non-commercial use and may not be used for any purpose other than to identify prospective properties consumers may be interested in purchasing.</p>
         <p>Based on information from the Austin Board of REALTORS® (alternatively, from ACTRIS) from <?=$dt->format('M d Y, H:i:s');?>. Neither the Board nor ACTRIS guarantees or is in any way responsible for its accuracy. The Austin Board of REALTORS®, ACTRIS and their affiliates provide the MLS and all content therein “AS IS” and without any warranty, express or implied. Data maintained by the Board or ACTRIS may not reflect all real estate activity in the market.</p>
-        <p>&copy; <?=date('Y')?> Fuse Realty. All rights reserved. <a href="#">Privacy + Terms</a></p>
+        <p>&copy; <?=date('Y')?> Fuse Realty. All rights reserved. <a href="<?=get_permalink(1197)?>">Privacy + Terms</a></p>
     </div>
 </footer>
 
